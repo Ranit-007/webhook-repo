94a971705b20702da825c594c52c183d2c1dd89a
RANIT PAL <ranitpal33@gmail.com>
2025-06-04 15:57:46 +0530

    Initial commit for webhook-repo

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+from datetime import datetime, timezone
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+# MongoDB connection
+try:
+    client = MongoClient(os.getenv("MONGO_URI"))
+    client.admin.command('ping')
+    print("Successfully connected to MongoDB!")
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+    exit(1)
+
+db = client.webhook_db
+events_collection = db.events
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    event_type = request.headers.get('X-GitHub-Event')
+    payload = request.json
+
+    print("📬 Webhook received:")
+    print("Event type:", event_type)
+    print("Payload:", payload)
+
+    if event_type not in ['push', 'pull_request']:
+        return jsonify({'error': 'Unsupported event type'}), 400
+
+    event_data = {
+        'event_type': event_type,
+        'timestamp': datetime.now(timezone.utc).isoformat()
+    }
+
+    if event_type == 'push':
+        event_data.update({
+            'author': payload['pusher']['name'],
+            'to_branch': payload['ref'].split('/')[-1]
+        })
+    elif event_type == 'pull_request':
+        event_data.update({
+            'author': payload['pull_request']['user']['login'],
+            'from_branch': payload['pull_request']['head']['ref'],
+            'to_branch': payload['pull_request']['base']['ref']
+        })
+
+    events_collection.insert_one(event_data)
+    return jsonify({'status': 'success'}), 200
+
+
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = list(events_collection.find({}, {'_id': 0}).sort('timestamp', -1))
+    return jsonify(events)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/fake')
+def insert_fake_event():
+    test_event = {
+        "event_type": "push",
+        "author": "Ranit-007",
+        "to_branch": "main",
+        "timestamp": datetime.now(timezone.utc).isoformat()
+    }
+    events_collection.insert_one(test_event)
+    return "Fake event inserted!"
+
+if __name__ == '__main__':
+    app.run(debug=True) 
